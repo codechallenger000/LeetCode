58cca4ba8fc8749e3d1c4327021cbb34b52ff28a
challenger-rise-it <jason.lee.020115@gmail.com>
2025-09-15 15:03:24 -0400

    Time: 3 ms (63.33%), Space: 57.1 MB (20%) - LeetHub

---
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.js
@@ -0,0 +1,16 @@
+const canBeTypedWords = (text, broken) => {
+    const brokenKeys = new Set(broken);
+    const words = text.split(" ");
+    let count = 0;
+
+    for (const word of words) {
+        for (const c of word) {
+            if (brokenKeys.has(c)) {
+                count++;
+                break;
+            }
+        }
+    }
+
+    return words.length - count;
+};
